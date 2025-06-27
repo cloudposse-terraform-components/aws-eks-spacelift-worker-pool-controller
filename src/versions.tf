1337adeb95bb34d4da5179855e22341ea25a3baa
Ben <ben.smith.developer@gmail.com>
2025-06-27 13:00:41 -0500

    chore(deps): restrict helm provider version to ~ 2.0.0 (#26)
    * pin helm to ~2.0
    
    * chore: update helm version constraints to allow 2.x range

--- src/versions.tf
+++ src/versions.tf
@@ -8,7 +8,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.0"
+      version = ">= 2.0.0, < 3.0.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
